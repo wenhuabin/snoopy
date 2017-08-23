e17318e49942c58af9ff4fa2ca47257d57fcd9b5
huabinwen <huabinwen@gmail.com>
2017-08-23 21:59:15 +0800

    add basic

---
+++ pages/basic/basic.js
@@ -0,0 +1,10 @@
+//basic.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
+  },
+})
