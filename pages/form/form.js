cc0469d7d87cef37fe188052791f19814a9eaf61
行旅書讀 <huabinwen@gmail.com>
2017-08-25 09:42:10 +0800

    add key and pick callback function

--- pages/form/form.js
+++ pages/form/form.js
@@ -106,6 +106,12 @@ var pageObject = {
     region: ['广东省', '广州市', '海珠区'],
     customItem: '全部',
   },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
   bindMultiPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
